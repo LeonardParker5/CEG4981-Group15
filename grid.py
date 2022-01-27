2cf9ecad9177b741e4d3d535d3eea0cf4a12e727
Chappell Jones <jonescl2012@gmail.com>
2022-01-27 09:25:53 -0500

    Revised grid
    The grid is 100 x 100 with one as edges.

--- grid.py
+++ grid.py
@@ -1,8 +1,7 @@
-grid = []
-for i in range (100):
-    grid.append([])
-    for j in range(1, 101):
-        grid[i].append(j)
-        
+import numpy as matrix
+
+grid = matrix.ones((100,100))
+grid[1:-1, 1:-1]=0
+
 print("100 X 100 grid with numbers:")
 print(grid)
